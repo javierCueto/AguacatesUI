e6f16688b4b0129e71a28583ef1dca83f6f9f202
Javier <javelie@outlook.com>
2020-05-15 16:12:47 -0600

    Initial Commit

---
+++ AguacatesUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AguacatesUI
+//
+//  Created by José Javier Cueto Mejía on 15/05/20.
+//  Copyright © 2020 Pozolx. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
